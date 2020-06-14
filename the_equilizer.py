135e7021803af70343dc354cb5403dd78ea55a5f
Soumik <soumik.ghosh1998@gmail.com>
2020-06-15 03:17:16 +0530

    updated the file to fix the write over issue

--- the_equilizer.py
+++ the_equilizer.py
@@ -1,21 +1,43 @@
 import os
 import shutil
 
-dir1 = "E:/songs"
-dir2 = "E:/Photos/Misc"
-dir3 = "E:/Book and documents"
-dir4 = "E:/Softwares"
-dir5 = "E:/Photos/Videos/Misc"
-dir6 = "E:/Compressed"
+src_dir = "download folder location"
+#usally C:\User\{username}\Downloads
+
+# file location directory 
+# example E:\newfolder
+
+
+dir1 = "File locations"
+dir2 = "File locations"
+dir3 = "File locations"
+dir4 = "File locations"
+dir5 = "File locations"
+dir6 = "File locations"
+dir7 = "File locations"
+dir8 = "File locations"
+
+
+
 
 def moveto(dst):
-    return lambda src: shutil.move(src, dst)
+	filename = os.path.basename(src_dir)
+	dest = os.path.join(dst,filename)
+	try:
+		return lambda src: shutil.move(src, dest)
+	except:
+		print(e)
+    	
+
+
+
 
 action = {
     'pdf': moveto(dir3),
     'docx': moveto(dir3),
     'exe': moveto(dir4),
     'jpg': moveto(dir2),
+    'JPG': moveto(dir2),
     'png': moveto(dir2),
     'gif': moveto(dir2),
     'torrent': os.remove,
@@ -25,10 +47,12 @@ action = {
     'wav': moveto(dir1),
     'zip': moveto(dir6),
     'jpeg': moveto(dir2),
+    'srt': os.remove,
 
 }
+
 item = 0
-src_dir = 'C:/Users/Soumik/Downloads'
+
 for file in os.listdir(src_dir):
     ext = os.path.splitext(file)[1][1:]
     if ext in action:
